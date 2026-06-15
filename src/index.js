9d8bbbf8ca5b76c510b64c1bceec8fc8929f8e74
Kartik Tomar <tomarkartik910@gmail.com>
2026-06-15 17:20:57 +0530

    database connected

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import dns from 'dns';        //this is the line of code which helps to solve my error (which lasts for 1 day)
+
+dns.setServers([
+  '1.1.1.1',
+  '8.8.8.8'
+]);
+
+
+
+
+// require('dotenv').config({path: './env'})--one way to configure dotenv (code works fine with this statement only but it spoils the consistency of the code so we write another method to configure dotenv which also dont spoils the consistency of the code)
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./.env'
+})
+
+console.log(process.env.MONGODB_URI)
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+//another way to connect to the databse
+/*
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants"
+
+import express from "express"
+const app = express()
+
+( async() => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",() => {
+            console.log("ERROR: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,() => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ",error)
+        throw error
+    }
+})()
+
+*/
