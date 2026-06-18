d5cf0578608ace3f9ec2749bc43dbbb893e59dba
Kartik Tomar <tomarkartik910@gmail.com>
2026-06-18 17:32:42 +0530

    all setup completed and also make an api and also test it

--- src/app.js
+++ src/app.js
@@ -13,4 +13,14 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))        //to handle dat
 app.use(express.static("public"))      //to handle the images and videos on the server itself
 app.use(cookieParser())   //it is for the middlewares configuration
 
+
+//routes import
+
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+//how our URL looks-- http://localhost:3000/api/v1/users/register
+
 export {app}
