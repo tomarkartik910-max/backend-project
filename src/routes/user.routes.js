f53261cd46819adba35ef796f2fd8244b91bf765
Kartik Tomar <tomarkartik910@gmail.com>
2026-06-23 18:12:05 +0530

    add user management routes: change password, get current user, update account details, and manage avatars

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import registerUser, { loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js"
+import registerUser, { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,5 +25,14 @@ router.route("/login").post(loginUser)
 //secured routes  --  will access only when user is logged in
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)  //patch is because only the selected fields to be update not the complete 
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)    //here data is coming in the URL
+router.route("/history").get(verifyJWT,getWatchHistory)
+
 
 export default router
